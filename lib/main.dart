5e28c9bb542eaaa0ca26ddc7efd9572ca44ab1e3
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2023-07-25 16:41:47 +0200

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nav_app_ecomputer/core/routes/app_router.dart';
+import 'package:nav_app_ecomputer/firebase_options.dart';
 
 import 'core/locator/locator.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupLocator();
   runApp(const MainApp());
 }
