f2caca9ca5515c6d952b01018f403639cc840423
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2023-07-28 16:23:20 +0200

    implement init

--- lib/modules/auth/ui/pages/welcome_page.dart
+++ lib/modules/auth/ui/pages/welcome_page.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nav_app_ecomputer/core/locator/locator.dart';
 import 'package:nav_app_ecomputer/core/routes/app_router.dart';
+import 'package:nav_app_ecomputer/modules/start/cubits/cubit/start_cubit.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.goNamed(AppRoutes.login.name);
-        },
-        child: const Icon(
-          Icons.arrow_forward_ios,
-        ),
-      ),
-      body: const Center(
-        child: Text('Bienvenid@'),
-      ),
+    return BlocBuilder<StartCubit, StartState>(
+      bloc: locator<StartCubit>()..init(),
+      builder: (context, state) {
+        return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              context.goNamed(AppRoutes.login.name);
+            },
+            child: const Icon(
+              Icons.arrow_forward_ios,
+            ),
+          ),
+          body: const Center(
+            child: Text('Bienvenid@'),
+          ),
+        );
+      },
     );
   }
 }
